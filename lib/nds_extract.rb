591fa91ba3532e78ab9722a556dcf8922cdcdbe1
Taevon Jordan <taevonjordan1@gmail.com>
2020-03-12 20:25:12 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -21,5 +21,11 @@ end
 # { directorOne => allTheMoneyTheyMade, ... }
 def directors_totals(nds)
   result = {}
-  
+    director_index = 0
+  while director_index < nds.length do
+    current_director = nds[director_index][:name]
+    result[current_director] = gross_for_director( nds[director_index] )
+    director_index += 1
+  end
+  result
 end
